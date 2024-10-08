2d981c27046ee7ac4bf9d6e4e8460e701c2f4f90
SJM <1206127263@qq.com>
2024-10-08 10:29:32 +0800

    添加sysinfo在user.h里面，并在makefile里面添加编译

--- kernel/sysinfo.h
+++ kernel/sysinfo.h
@@ -1,4 +1,5 @@
-struct sysinfo {
-  uint64 freemem;   // amount of free memory (bytes)
-  uint64 nproc;     // number of process
+struct sysinfo
+{
+  uint64 freemem; // amount of free memory (bytes) 空闲内存大小
+  uint64 nproc;   // number of process 当前系统进程数量的统计
 };
