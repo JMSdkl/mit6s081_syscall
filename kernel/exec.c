73c8a1e70379e444135b785fa025c8f626426165
SJM <1206127263@qq.com>
2024-10-21 21:09:40 +0800

    添加 vmprintf 递归访问每个页表

--- kernel/exec.c
+++ kernel/exec.c
@@ -118,7 +118,7 @@ int exec(char *path, char **argv)
   p->trapframe->sp = sp;         // initial stack pointer
   proc_freepagetable(oldpagetable, oldsz);
   if (p->pid == 1)
-    vmprintf(p->pagetable);
+    vmprint(p->pagetable);
   return argc; // this ends up in a0, the first argument to main(argc, argv)
 
 bad:
